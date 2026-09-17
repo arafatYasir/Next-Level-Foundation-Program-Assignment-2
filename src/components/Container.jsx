525ce072d7c31de1dfa04e85f3eab9a9a6e3052f
arafatYasir <arafat.yasir1305@gmail.com>
2026-09-17 09:41:58 +0600

    feat(ui): implement homepage with responsive design

---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+const Container = ({ children }) => {
+    return (
+        <div className="container mx-auto px-6 sm:px-0">{children}</div>
+    )
+}
+
+export default Container
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link, NavLink } from "react-router"
+import Container from "./Container"
+
+const Navbar = () => {
+    return (
+        <header className="sticky top-0 w-full bg-[#4c3f9a]/40 border-b border-b-white/40 backdrop-blur-sm">
+            <Container>
+                <nav className="flex items-center justify-between py-3">
+                    {/* ---- Logo ---- */}
+                    <NavLink to="/" className="flex items-center gap-2">
+                        <img
+                            src="/logo.png"
+                            width={100}
+                            height={100}
+                            alt="MovieBox Logo"
+                            className="size-7 sm:size-9 pointer-events-none"
+                            fetchPriority="high"
+                        />
+                        <span className="text-xl sm:text-2xl font-bold">MovieBox</span>
+                    </NavLink>
+
+                    <Link to="/movies" className="text-lg sm:text-xl hover:text-[#ab8bff] transition-colors duration-100">All Movies</Link>
+                </nav>
+            </Container>
+        </header>
+    )
+}
+
+export default Navbar
