4cba2a772e48513865262a6c7b45ab862f41ea10
arafatYasir <arafat.yasir1305@gmail.com>
2026-09-17 08:59:09 +0600

    feat(ui): implement routing using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { Route, Routes } from "react-router"
+import HomePage from "./pages/HomePage"
+import MoviesPage from "./pages/MoviesPage"
+import RootLayout from "./layouts/RootLayout"
+
 const App = () => {
   return (
-    <div className="">App</div>
+    <>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
